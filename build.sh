d5486fe44ef573239893e5237423839ffcb3383b
djoslin <joslin_productions@hotmail.com>
2019-11-25 21:11:56 -0800

    Reorganized folder structure
    Now all *.c utilities are in src/utils/
    All stages remain directly in src/

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ rm ../bin/*
 echo "#############"
 echo "# Tokenizer #"
 echo "#############"
-gcc tokenizer.c symbols.c file.c -I include -o "../bin/tokenizer" -Wall -Werror -Wpedantic
+gcc tokenizer.c utils/symbols.c utils/file.c -I include -o "../bin/tokenizer" -Wall -Werror -Wpedantic
 ../bin/tokenizer $1
 
 echo ""
 echo "##########"
 echo "# Parser #"
 echo "##########"
-gcc parser.c symbols.c file.c -I include -o "../bin/parser" -Wall -Werror -Wpedantic
+gcc parser.c utils/symbols.c utils/file.c -I include -o "../bin/parser" -Wall -Werror -Wpedantic
 ../bin/parser $1
 
 # only generates graph if graphviz is installed
@@ -23,7 +23,7 @@ if hash dot 2>/dev/null; then
   echo "#########"
   echo "# Graph #"
   echo "#########"
-  gcc graphviz.c symbols.c file.c nodes.c -I include -o "../bin/graph" -Wall -Werror -Wpedantic
+  gcc graphviz.c utils/symbols.c utils/file.c utils/nodes.c -I include -o "../bin/graph" -Wall -Werror -Wpedantic
   ../bin/graph $1
   dot -Tpng ../bin/$1.dot > ../bin/$1.png
 fi
@@ -32,13 +32,13 @@ echo ""
 echo "###########"
 echo "# Codegen #"
 echo "###########"
-gcc codegen.c symbols.c file.c nodes.c -I include -o "../bin/codegen" -Wall -Werror -Wpedantic
+gcc codegen.c utils/symbols.c utils/file.c utils/nodes.c -I include -o "../bin/codegen" -Wall -Werror -Wpedantic
 ../bin/codegen $1
 
 echo ""
 echo "######"
 echo "# VM #"
 echo "######"
-gcc vm.c symbols.c file.c -I include -o "../bin/vm" -Wall -Werror -Wpedantic
+gcc vm.c utils/symbols.c utils/file.c -I include -o "../bin/vm" -Wall -Werror -Wpedantic
 ../bin/vm $1
 
